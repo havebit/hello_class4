800d9b803d438052ad0d17af143b77ded63e5f8f
Pallat Anchaleechamaikorn <pallat.anc@cjexpress.co.th>
2022-08-13 11:17:55 +0700

    refactor any int input to string

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -1,23 +1,19 @@
 package foobar
 
+import "strconv"
+
 func Say(n int) string {
-	if n == 7 {
-		return "7"
-	}
+
 	if n == 5 {
 		return "Bar"
 	}
-	if n == 4 {
-		return "4"
-	}
+
 	if n == 6 {
 		return "Foo"
 	}
 	if n == 3 {
 		return "Foo"
 	}
-	if n == 2 {
-		return "2"
-	}
-	return "1"
+
+	return strconv.Itoa(n)
 }
